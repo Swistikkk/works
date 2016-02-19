3e3325280fabfe7de43e98fc85c731fdc42b8b13
swistikkk <oberemkovictor@gmail.com>
2016-02-19 16:02:59 +0400

    need to do some elements and adaptive for site + has a lot of useless files

--- now/projects/Podium Style/html/js/functions.js
+++ now/projects/Podium Style/html/js/functions.js
@@ -3,4 +3,11 @@ $(document).ready(function () {
     $('.good-card').click(function () {
         $('.good-card').children('.good-sizes').slideToggle();
     });
+    $('.your-class').slick({
+    	dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+    });
 });
